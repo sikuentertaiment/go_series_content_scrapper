6d0e256ba4ce89a6e5dff2850ef9a84813873662
sikuentertaiment <gemalagifrominfinitydreams@gmail.com>
2024-07-20 15:53:46 +0700

    working on things

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const fs = require('fs');
+const readline = require('readline');
 
-const url = 'https://google.com';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const woody = {
 	config:{
@@ -31,8 +36,10 @@ const woody = {
 	}
 }
 
+// working on details series
+
 woody.init({
-	url:'https://tv5.anikor.pics/series/komedi-kacau-2024/',
+	url:'https://anikun.sbs/anime/b-the-beginning/',
 	setConfig(p){
 		p.config.request.url = this.url;
 	},
@@ -46,9 +53,14 @@ woody.init({
 		})
 
 		// working on series props
+		$('.info-content .spe span').each((index,element)=>{
+			const props = $(element).text().split(': ');
+			props[1] = props[1].split(', ');
+			series[props[0]] = props[1].length > 1 ? props[1] : props[0];
+		})
 
 		// working on series sinopsis
-		$('.entry-content p').each((index,element)=>{
+		$('.entry-content').each((index,element)=>{
 			series.sinopsis = $(element).text();	
 		})
 
@@ -74,3 +86,147 @@ woody.init({
 	    console.log(series)
 	}
 });
+
+const work =  async ()=>{
+
+	woody.init({
+		url:'https://anikun.sbs/anime/b-the-beginning/',
+		setConfig(p){
+			p.config.request.url = this.url;
+		},
+		sf($){
+			const series = {
+				links:{}
+			};
+			// working series title
+			$('.entry-title').each((index,element)=>{
+				series.title = $(element).text();
+			})
+
+			// working on series props
+			$('.info-content .spe span').each((index,element)=>{
+				const props = $(element).text().split(': ');
+				props[1] = props[1].split(', ');
+				series[props[0]] = props[1].length > 1 ? props[1] : props[0];
+			})
+
+			// working on series sinopsis
+			$('.entry-content').each((index,element)=>{
+				series.sinopsis = $(element).text();	
+			})
+
+			// working on series thumbnail and icon
+			$('.thumb img').each((index,img)=>{
+				series.thumb_img = $(img).attr('src');
+			})
+
+			// working on downloa links
+			$('.soraurlx').each((index, element) => {
+				const item = {};let resolution;
+				$(element).children((childIndex,child)=>{
+					child = $(child);
+					const tag_ = child.prop('tagName');
+					if(tag_==='STRONG'){
+						resolution = child.text();
+					}else{
+						item[child.text()] = child.attr('href');
+					}
+				})
+				series.links[resolution] = item;
+		    });
+		    console.log(series)
+		}
+	});
+}
+work();
+
+
+// now its B
+
+
+// working getting drama list
+// let data = [];
+// let process_ = true;
+// let Char,Page,Base,Index = 0;
+// const geturl = (char, index)=>{
+// 	if(!index)return `${Base}?show=${char}`;
+// 	return `${Base}page/${index+1}/?show=${char}`;
+// }
+// const work = async ()=>{
+// 	if(!data.length){
+// 		const res = await new Promise((resolve,reject)=>{
+// 			fs.readFile('./file_list.data', 'utf8', (err, data_) => {
+// 			  if (err) {
+// 			    return resolve(false);
+// 			  }
+// 			  data = JSON.parse(data_);
+// 			  resolve(true);
+// 			});
+// 		})
+// 		if(!res){
+// 			console.log('Fail to read old data.')
+// 			return rl.close();
+// 		}
+// 	}
+// 	if(!Base){
+// 		await new Promise((resolve,reject)=>{
+// 			rl.question('Input Base Url: ',(ans)=>{
+// 				Base = ans;
+// 				resolve();
+// 			})
+// 		})
+// 	}
+// 	if(!Char){
+// 		await new Promise((resolve,reject)=>{
+// 			rl.question('Input Char: ',(ans)=>{
+// 				Char = ans;
+// 				resolve();
+// 			})	
+// 		})
+// 	}
+// 	if(Char==='finish'){
+// 		await new Promise((resolve,reject)=>{
+// 			fs.writeFile('./file_list.data', JSON.stringify(data), (err) => {
+// 			  if (err) {
+// 			    console.error('Error writing file:', err);
+// 			  } else {
+// 			    console.log('File written successfully');
+// 			  }
+// 			  resolve();
+// 			});
+// 		})
+// 		console.log('Program finished:',data.length,'found.');
+// 		return rl.close();
+// 	}
+// 	if(!Page){
+// 		await new Promise((resolve,reject)=>{
+// 			rl.question('Input Page: ',(ans)=>{
+// 				Page = Number(ans);
+// 				resolve();
+// 			})	
+// 		})
+// 	}
+// 	woody.init({
+// 		url:geturl(Char, Index),
+// 		setConfig(p){
+// 			p.config.request.url = this.url;
+// 			console.log(this.url);
+// 		},
+// 		sf($){
+// 			$('.bsx a').each((index,element)=>{
+// 				data.push($(element).attr('href'));
+// 			})
+// 			Index += 1;
+// 			console.log(data.length)
+// 			if(Index === Page){
+// 				Page = null;
+// 				Char = null;
+// 				Index = 0;
+// 			};
+// 			work();		
+// 		}
+// 	});
+// }
+// console.clear();
+// work();
+
